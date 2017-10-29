eec6e182781c9614c765990518bf5a615e93cd59
hadjowernure <vlayslav.dudar@nure.ua>
2017-10-29 11:36:03 +0200

    add Exception handling to connection factory

--- src/main/java/ua/nure/kn_15_6/dudar/db/ConnectionFactoryImpl.java
+++ src/main/java/ua/nure/kn_15_6/dudar/db/ConnectionFactoryImpl.java
@@ -1,21 +1,23 @@
 package ua.nure.kn_15_6.dudar.db;
 
+import ua.nure.kn_15_6.dudar.Constants;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactoryImpl implements ConnectionFactory {
-    private static String DRIVER = "org.hsqldb.jdbcDriver";
-    private static String URL = "jdbc:hsqldb:file:/res/db/userdb";
-    private static String USER = "SA";
-    private static String PASS = "";
+    private static final String DRIVER = "org.hsqldb.jdbcDriver";
+    private static final String URL = "jdbc:hsqldb:file:/res/db/userdb";
+    private static final String USER = "SA";
+    private static final String PASS = "";
 
     @Override
     public Connection createConnection() throws SQLException {
         try {
             Class.forName(DRIVER);
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException("ERROR: failed to load HSQLDB JDBC driver.");
+            throw new RuntimeException(Constants.ERROR_TO_LOAD_JDBC);
         }
 
         return DriverManager.getConnection(URL, USER, PASS);
