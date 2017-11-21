e1277dfd1950817381d41b04d4f08e13db164569
hadjowernure <vlayslav.dudar@nure.ua>
2017-11-21 13:17:31 +0200

    create browse servlet and jsp page

---
+++ src/main/java/ua/nure/kn_15_6/dudar/view/web/BrowseServlet.java
@@ -0,0 +1,30 @@
+package ua.nure.kn_15_6.dudar.view.web;
+
+import ua.nure.kn_15_6.dudar.User;
+import ua.nure.kn_15_6.dudar.db.DaoFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Collection;
+
+public class BrowseServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+        browse(req, resp);
+    }
+
+    private void browse(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
+        try {
+            Collection<User> users = DaoFactory.getInstance().getUserDao().findAll();
+            req.getSession().setAttribute("users", users);
+            req.getRequestDispatcher("/browse.jsp").forward(req, resp);
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
+}
